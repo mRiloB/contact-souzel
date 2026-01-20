a8eeb942ea01d7d156b0fe8028c1cd9d92b3bac8
M. Benicio <m.benicio@outlook.com>
2026-01-19 21:25:28 -0300

    refactor: change background image;

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@ body {
   height: 100%;
   width: 100%;
   background-color: #011a2e;
-  /* background: url('@/assets/img/ecstudio3d_fundo.jpg');
+  background: url('@/assets/img/ecstudio3d_fundo.jpg');
   background-repeat: no-repeat;
   background-position: center;
-  background-size: cover; */
+  background-size: cover;
 }
 </style>
